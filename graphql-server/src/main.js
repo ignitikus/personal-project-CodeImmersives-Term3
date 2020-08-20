1c5b68df4aeabb203e79e690c842995c6e2df0df
Ubuntu <ubuntu@ip-10-0-0-155.ec2.internal>
2020-08-20 02:30:11 +0000

    merge fix

--- graphql-server/src/main.js
+++ graphql-server/src/main.js
@@ -12,16 +12,11 @@ require('dotenv').config()
 const app = express()
 app.use(cookieParser())
 app.use(cors({
-<<<<<<< HEAD
   origin: 'http://nikokim.com',
  // origin: '*',
  // methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
  // allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
-=======
-    origin: 'http://localhost:3000',
-    credentials: true
->>>>>>> 4b23f474451bad52825b74c7547e662dd3aeee00
 }))
 app.disable('x-powered-by')
 
